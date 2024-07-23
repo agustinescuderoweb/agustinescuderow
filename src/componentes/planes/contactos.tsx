b052310c0d19dfe2f244d05b9bb3681cfa356aef
Agustín Escudero Web <124199392+agustinescuderoweb@users.noreply.github.com>
2024-07-22 23:39:12 -0300

    Update contactos.tsx

--- src/componentes/planes/contactos.tsx
+++ src/componentes/planes/contactos.tsx
@@ -13,8 +13,8 @@ function contactos() {
     <h1>Enviáme un Mensaje</h1>
     <h2>Seguí mis redes Sociales</h2>
     <div className={styles.icon}>
-    <a href='https://wa.link/elp2e3'><Image className={styles.icons} src={whatsaap}></Image></a>
-    <a href='https://www.instagram.com/agustinescuderoweb/'><Image className={styles.icons} src={instagram}></Image></a>
+    <a href='https://wa.link/elp2e3'><Image className={styles.icons} src={whatsaap} alt="WhatsApp"></Image></a>
+    <a href='https://www.instagram.com/agustinescuderoweb/'><Image className={styles.icons} src={instagram} alt="Instagram"></Image></a>
     </div>
     </div>
   )
