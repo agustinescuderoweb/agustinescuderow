53dbed7967a7157c056b2ae86478081f3cdc5f87
Agustin <agustinescuderoweb@gmail.com>
2024-10-02 21:13:08 -0300

    nuevo commit

--- src/componentes/quiensoy/blog.tsx
+++ src/componentes/quiensoy/blog.tsx
@@ -6,12 +6,12 @@ function blog() {
     <div className={styles.contenedor}>
         <h1 className={styles.titulo}>Blog</h1>
         <div className={styles.blog}>
-        <h1>Ventajas de Tener un Sitio Web</h1>
-        <h1>Desarrollo Web con Código vs Wordpress</h1>
-        <h1>¿Qué es un Dominio y un Hosting</h1>
+        <a href='../../blog/ventajas'><h1>Ventajas de tener un Sitio Web</h1></a>
+        <a href='../../blog/desarrollo'><h1>Desarrollo Web con Código vs Wordpress</h1></a>
+        <a href='../../blog/dominio'><h1>¿Qué es un Dominio y un Hosting?</h1></a>
         </div>
         <div className={styles.boton}>
-        <button>Ver Más Articulos</button>
+        <a href='../../blog'><button>Ver Más Articulos</button></a>
         </div>    
     </div>
   )
