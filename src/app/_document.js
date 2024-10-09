1a6bef2a3c05f846b121c1c65eb571cb7c8bb72e
Agustin <agustinescuderoweb@gmail.com>
2024-10-09 18:18:30 -0300

    nuevo comit

--- src/app/_document.js
+++ src/app/_document.js
@@ -6,6 +6,20 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/../public/icon/favicon.ico" />
+         {/* Google Analytics */}
+         <script async src="https://www.googletagmanager.com/gtag/js?id=G-81Q9W2LL4J"></script>
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-81Q9W2LL4J', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
