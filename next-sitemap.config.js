d6b47f9e49aca15aa26d8adcd807db35fb2aa6c5
Agustin <agustinescuderoweb@gmail.com>
2025-06-03 10:12:11 -0300

    Nuevo

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://agustinescuderoweb.com',
+  generateRobotsTxt: true, // También genera robots.txt
+  sitemapSize: 5000,
+  outDir: './public', 
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import Head from 'next/head'
 import Menu from '../../componentes/planes/menu'
 import Blog from '../../componentes/blog/blog'
 import Redes from '../../componentes/contactos/redes'
 
-function blog() {
+function BlogPage() {
   return (
-    <div>
-      <Menu />
-      <Blog />
-      <Redes />
-    </div>
+    <>
+      <Head>
+        <title>Blog de Desarrollo Web | Tips y Tutoriales por Agustín Escudero</title>
+        <meta
+          name="description"
+          content="Descubre los mejores consejos, tutoriales y novedades sobre desarrollo web. Aprende a crear sitios profesionales y optimizados con Agustín Scudero."
+        />
+      </Head>
+
+      <div>
+        <Menu />
+        <Blog />
+        <Redes />
+      </div>
+    </>
   )
 }
 
-export default blog
+export default BlogPage
