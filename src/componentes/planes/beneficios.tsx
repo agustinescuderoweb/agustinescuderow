bd69cd8f1d071d7a66804c89465e2180c4fdfb09
Agustin <agustinescuderoweb@gmail.com>
2025-07-02 20:54:32 -0300

    cambio de letras

--- src/componentes/planes/beneficios.tsx
+++ src/componentes/planes/beneficios.tsx
@@ -20,7 +20,7 @@ function beneficios() {
       </div>
       <div className={style.cajas}>
         <div className={style.number}><h1>03</h1></div>
-        <div className={style.textos}><h1>Control total del contenido</h1>
+        <div className={style.textos}><h1>Control total</h1>
         <h2>No dependés de algoritmos ni de cambios en redes sociales. Podés mostrar tu marca como querés.</h2></div>
         
       </div>
@@ -28,7 +28,7 @@ function beneficios() {
       <div className={style.filas}>
       <div className={style.cajas}>
         <div className={style.number}><h1>04</h1></div>
-        <div className={style.textos}><h1>Mejor posicionamiento en Google (SEO)</h1>
+        <div className={style.textos}><h1>Mejor Google (SEO)</h1>
         <h2>Una web bien hecha puede aparecer en buscadores y atraer personas interesadas sin pagar publicidad.</h2></div>
        
       </div>
@@ -40,7 +40,7 @@ function beneficios() {
       </div>
       <div className={style.cajas}>
         <div className={style.number}><h1>06</h1></div>
-        <div className={style.textos}><h1>Velocidad y experiencia de usuario</h1>
+        <div className={style.textos}><h1>Velocidad y experiencia</h1>
         <h2>Una web rápida y clara mejora la experiencia y hace que las personas se queden más tiempo.</h2></div>
         
       </div>
@@ -48,7 +48,7 @@ function beneficios() {
       <div className={style.filas}>
       <div className={style.cajas}>
         <div className={style.number}><h1>07</h1></div>
-        <div className={style.textos}><h1>Adaptación a dispositivos móviles</h1>
+        <div className={style.textos}><h1>Adaptación a móviles</h1>
          <h2>El 90% entra desde el celular. Si tu web no está adaptada, perdés oportunidades.</h2></div>
         
       </div>
@@ -60,7 +60,7 @@ function beneficios() {
       <div className={style.cajas}>
       <div className={style.number}><h1>09</h1></div>
       <div className={style.textos}>
-        <h1>Medición y análisis de resultados</h1>
+        <h1>Medición de resultados</h1>
         <h2>Con herramientas como Google Analytics podés saber cuántas personas te visitan, desde dónde llegan y qué les interesa.</h2>
       </div>
       </div>
